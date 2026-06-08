c701bc0afe54b3473001871954562dd1dea4588e
kototo123 <kototo123@users.noreply.github.com>
2026-06-08 18:19:49 +0800

    fix: UserPrefsMapper加@Mapper注解

--- time-is-money-backend/src/main/java/com/timoney/mapper/UserPrefsMapper.java
+++ time-is-money-backend/src/main/java/com/timoney/mapper/UserPrefsMapper.java
@@ -2,6 +2,8 @@ package com.timoney.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.timoney.entity.UserPrefs;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface UserPrefsMapper extends BaseMapper<UserPrefs> {
 }
